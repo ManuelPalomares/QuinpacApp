7b4ea70562e7f116e2736e05ac37ee6ac4431fbf
ManuelPalomares <mpalomar@COCLOML1C73S32.mgd.mrshmc.com>
2015-11-17 11:48:29 -0500

    Se crear proyecto
    Creacion de proyecto

---
+++ app/src/main/java/com/web/webmapsoft/barcodescanner/ActividadPrincipal.java
@@ -0,0 +1,37 @@
+package com.web.webmapsoft.barcodescanner;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by mpalomar on 16/11/15.
+ */
+public class ActividadPrincipal extends Activity {
+
+    private Button btnLlenado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        btnLlenado = (Button) findViewById(R.id.x_llenado);
+
+        btnLlenado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirFormaLLenado();
+
+            }
+        });
+
+    }
+
+    public void abrirFormaLLenado(){
+
+    }
+}
